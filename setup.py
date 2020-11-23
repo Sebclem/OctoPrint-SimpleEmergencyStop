c4dbe534555f4589110d9cbcdb823c73515740d2
Sebastien Clement <sebastien.clement@univ-pau.fr>
2020-11-23 12:49:11 +0100

    :pencil2: Update Github username

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Clément Sébastien"
 plugin_author_email = "seb6596@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/BrokenFire/OctoPrint-SimpleEmergencyStop"
+plugin_url = "https://github.com/Sebclem/OctoPrint-SimpleEmergencyStop"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
